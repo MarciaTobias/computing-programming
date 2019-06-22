e517df8acbc4092ee262dc3a7e3be19d02c6a1a3
Marcia Tobias <marciaatobiass@gmail.com>
2019-06-22 19:35:59 +0100

    Practical 1 and GitIgonore
    Adding student details and git ignore file.

---
+++ Practical 1/1. StudentDetails/src/StudentDetails.java
@@ -0,0 +1,21 @@
+/* Write a Java program in a file called “StudentDetails.java” capable of writing three lines to the console upon
+execution displaying your student information as follows (replace with your details):
+
+* Name: Joe Bloggs
+* Student Number: 12345678
+* Town: Stillorgan
+*/
+
+public class StudentDetails {
+
+    public static void main(String[] args) {
+
+        System.out.println("Joe Bloggs");
+        System.out.println("Student Number: 12345678");
+        System.out.println("Town: Stillorgan");
+    }
+}
+
+
+
+
