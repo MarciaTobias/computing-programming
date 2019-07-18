40a9b22b9fb4316920f7c57806f8ce7ee290f126
Marcia Tobias <marciaatobiass@gmail.com>
2019-07-18 21:32:16 +0100

    Adding workbook LeapYear from Practical 4.

--- Practical 4/3. LeapYears/src/LeapYears.java
+++ Practical 4/3. LeapYears/src/LeapYears.java
@@ -1,9 +1,9 @@
 /**
- * Practical 4 Question 2
+ * Practical 4 Question 3
  *
- *      Write a program that lets the user enter the loan amount and loan period
- *      in number of years and displays the monthly and total payments for each
- *      interest rate starting from 5% to 8%, with an increment of 1/8%.
+ *      Write a program that displays all the leap years,
+ *      ten per line, in the twenty-first century (from 2001 to 2100),
+ *      separated by exactly one space.
  *
  * Created by Marcia Tobias on 23/10/2018.
  * */
@@ -11,20 +11,22 @@
 public class LeapYears {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-        //declaration of the counter
+        // Declaration of the counter.
         int count = 0;
 
-        //the range of years from 2001 to 2100
+        // Print the title of the table.
+        System.out.println("\n\t\t** Leap Years from 2001 to 2100 **\n");
+
+        // The range of years from 2001 to 2100.
         for (int myYear = 2001; myYear <= 2100; myYear++) {
 
-            //calculation to get the leap year
+            // Calculation to get the leap year.
             if ((myYear % 4 == 0 && myYear % 100 !=0) || (myYear % 400 == 0)) {
 
                 count++;
 
-                //print the leap years ten per line
+                // Print the leap years ten per line.
                 System.out.print(myYear + (count % 10 == 0 ? "\n" : " "));
             }
         }
